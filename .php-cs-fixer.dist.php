5fb32893a25b983acf35b4c70229989f5f887061
Tim Lochmüller <tim.lochmueller@hdnet.de>
2026-01-29 19:06:27 +0100

    feat: Add basic test structure

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
+use PhpCsFixer\Runner\Parallel\ParallelConfigFactory;
+
+return (new Config())
+    ->setParallelConfig(ParallelConfigFactory::detect())
+    ->setRiskyAllowed(true)
+    ->setRules([
+        '@PER-CS3.0' => true,
+        '@PER-CS3.0:risky' => true,
+        '@PHP8x3Migration' => true,
+        '@PHP8x3Migration:risky' => true,
+        '@PHPUnit10x0Migration:risky' => true,
+        'declare_strict_types' => true,
+        'no_unused_imports' => true
+    ])
+    ->setFinder(
+        (new Finder())
+            ->in(__DIR__)
+            ->exclude(['.Build'])
+    );
---
+++ Tests/Unit/AbstractTest.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Lochmueller\Seal\Tests\Unit;
+
+use TYPO3\TestingFramework\Core\Unit\UnitTestCase;
+
+abstract class AbstractTest extends UnitTestCase
+{
+    protected const TEST_ITERATIONS = 5;
+
+}
