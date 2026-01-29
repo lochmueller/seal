d0801b6948956c9a106dee6f22f31a942840a68e
Tim Lochmüller <tim.lochmueller@hdnet.de>
2026-01-29 19:15:19 +0100

    fix: First test

---
+++ Tests/Unit/Command/SchemaCommandTest.php
@@ -0,0 +1,185 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Lochmueller\Seal\Tests\Unit\Command;
+
+use CmsIg\Seal\EngineInterface;
+use Lochmueller\Seal\Command\SchemaCommand;
+use Lochmueller\Seal\Seal;
+use Lochmueller\Seal\Tests\Unit\AbstractTest;
+use PHPUnit\Framework\MockObject\MockObject;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use TYPO3\CMS\Core\Site\Entity\Site;
+use TYPO3\CMS\Core\Site\SiteFinder;
+
+class SchemaCommandTest extends AbstractTest
+{
+    private MockObject $siteFinderMock;
+
+    private MockObject $sealMock;
+
+    private SchemaCommand $subject;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->siteFinderMock = $this->createMock(SiteFinder::class);
+        $this->sealMock = $this->createMock(Seal::class);
+
+        $this->subject = new SchemaCommand($this->siteFinderMock, $this->sealMock);
+    }
+
+    public function testExecuteReturnsSuccessWithNoSites(): void
+    {
+        $this->siteFinderMock
+            ->expects(self::once())
+            ->method('getAllSites')
+            ->willReturn([]);
+
+        $inputMock = $this->createMock(InputInterface::class);
+        $outputMock = $this->createMock(OutputInterface::class);
+
+        $result = $this->invokeExecute($inputMock, $outputMock);
+
+        self::assertSame(Command::SUCCESS, $result);
+    }
+
+    public function testExecuteCreatesSchemaForEachSite(): void
+    {
+        $site1 = $this->createMock(Site::class);
+        $site2 = $this->createMock(Site::class);
+
+        $engine1 = $this->createMock(EngineInterface::class);
+        $engine2 = $this->createMock(EngineInterface::class);
+
+        $this->siteFinderMock
+            ->expects(self::once())
+            ->method('getAllSites')
+            ->willReturn([$site1, $site2]);
+
+        $this->sealMock
+            ->expects(self::exactly(2))
+            ->method('buildEngineBySite')
+            ->willReturnCallback(fn($site) => match ($site) {
+                $site1 => $engine1,
+                $site2 => $engine2,
+                default => throw new \LogicException('Unexpected site'),
+            });
+
+        $engine1->expects(self::once())->method('createSchema');
+        $engine2->expects(self::once())->method('createSchema');
+
+        $inputMock = $this->createMock(InputInterface::class);
+        $outputMock = $this->createMock(OutputInterface::class);
+
+        $result = $this->invokeExecute($inputMock, $outputMock);
+
+        self::assertSame(Command::SUCCESS, $result);
+    }
+
+    public function testExecuteCatchesExceptionAndContinues(): void
+    {
+        $site1 = $this->createMock(Site::class);
+        $site2 = $this->createMock(Site::class);
+
+        $engine1 = $this->createMock(EngineInterface::class);
+        $engine2 = $this->createMock(EngineInterface::class);
+
+        $this->siteFinderMock
+            ->expects(self::once())
+            ->method('getAllSites')
+            ->willReturn([$site1, $site2]);
+
+        $this->sealMock
+            ->expects(self::exactly(2))
+            ->method('buildEngineBySite')
+            ->willReturnCallback(fn($site) => match ($site) {
+                $site1 => $engine1,
+                $site2 => $engine2,
+                default => throw new \LogicException('Unexpected site'),
+            });
+
+        $engine1->expects(self::once())
+            ->method('createSchema')
+            ->willThrowException(new \Exception('Test exception'));
+
+        $engine2->expects(self::once())->method('createSchema');
+
+        $inputMock = $this->createMock(InputInterface::class);
+        $outputMock = $this->createMock(OutputInterface::class);
+
+        $result = $this->invokeExecute($inputMock, $outputMock);
+
+        self::assertSame(Command::SUCCESS, $result);
+    }
+
+    public function testExecuteLogsExceptionMessage(): void
+    {
+        $site = $this->createMock(Site::class);
+        $engine = $this->createMock(EngineInterface::class);
+        $logger = $this->createMock(LoggerInterface::class);
+
+        $this->siteFinderMock
+            ->expects(self::once())
+            ->method('getAllSites')
+            ->willReturn([$site]);
+
+        $this->sealMock
+            ->expects(self::once())
+            ->method('buildEngineBySite')
+            ->with($site)
+            ->willReturn($engine);
+
+        $engine->expects(self::once())
+            ->method('createSchema')
+            ->willThrowException(new \Exception('Schema creation failed'));
+
+        $logger->expects(self::once())
+            ->method('info')
+            ->with('Schema creation failed');
+
+        $this->subject->setLogger($logger);
+
+        $inputMock = $this->createMock(InputInterface::class);
+        $outputMock = $this->createMock(OutputInterface::class);
+
+        $result = $this->invokeExecute($inputMock, $outputMock);
+
+        self::assertSame(Command::SUCCESS, $result);
+    }
+
+    public function testExecuteHandlesExceptionFromBuildEngineBySite(): void
+    {
+        $site = $this->createMock(Site::class);
+
+        $this->siteFinderMock
+            ->expects(self::once())
+            ->method('getAllSites')
+            ->willReturn([$site]);
+
+        $this->sealMock
+            ->expects(self::once())
+            ->method('buildEngineBySite')
+            ->with($site)
+            ->willThrowException(new \Exception('Engine build failed'));
+
+        $inputMock = $this->createMock(InputInterface::class);
+        $outputMock = $this->createMock(OutputInterface::class);
+
+        $result = $this->invokeExecute($inputMock, $outputMock);
+
+        self::assertSame(Command::SUCCESS, $result);
+    }
+
+    private function invokeExecute(InputInterface $input, OutputInterface $output): int
+    {
+        $reflection = new \ReflectionMethod($this->subject, 'execute');
+
+        return $reflection->invoke($this->subject, $input, $output);
+    }
+}
