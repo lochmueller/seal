f79b1874987b495fad989cbefce4e4590ad13558
Tim Lochmüller <tim.lochmueller@hdnet.de>
2026-04-13 21:20:42 +0200

    chore: Change ext_emconf.php version to 1.1.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,7 +4,7 @@
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Search Engine Abstraction Layer',
     'description' => 'SEAL Search - Flexible integration of the Search Engine Abstraction Layer project',
-    'version' => '1.0.0',
+    'version' => '1.1.0',
     'category' => 'fe',
     'constraints' => [
         'depends' => [
